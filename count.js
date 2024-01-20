7b772f713255be2a914584497115b602d0d652fb
RicePot <114596831+TheRicePot@users.noreply.github.com>
2024-01-20 09:34:17 -0600

    f

--- count.js
+++ count.js
@@ -1,5 +1,5 @@
 // Set the date you want to countdown to in MM/DD/YYYY format
-const countDownDate = moment.tz("01/31/2024 00:00:00", "America/New_York").valueOf(); // Set your desired timezone
+const countDownDate = new Date("01/31/2024 00:00:00").getTime();
 
 // Set the reason code (1 for "abuse of platform", 2 for "toxicity")
 const reasonCode = 1; // Change this to the desired reason code
@@ -8,9 +8,9 @@ const reasonCode = 1; // Change this to the desired reason code
 function getReason(code) {
     switch (code) {
         case 1:
-            return "getting my ass cleaning like the cafeteria my mother";
+            return "Getting my ass cleaning like my mother";
         case 2:
-            return "Getting Website Blocked/Saying i made it";
+            return "Reporting Website to teachers/staff.";
         default:
             return "Reason not specified";
     }
@@ -31,11 +31,11 @@ const x = setInterval(function() {
     const reason = getReason(reasonCode);
 
     // Display the result with additional text
-    document.getElementById("countdown").innerHTML = `Reason for ban: ${reason} | Days remaining until unban: ${days}`;
+    document.getElementById("countdown").innerHTML = `Reason for ban: ${reason} | Days remaining until release: ${days}`;
 
     // If the countdown is over, display a message
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdown").innerHTML = `Please Wait until unban. (${reason})`;
+        document.getElementById("countdown").innerHTML = `The wait is over! (${reason})`;
     }
 }, 1000);
